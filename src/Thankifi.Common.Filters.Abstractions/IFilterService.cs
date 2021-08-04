fcc92654d26f2761032f297708d637c2baf20492
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-04 13:21:13 +0300

    feat: add new methods to IFilterService for bulk filtering

--- src/Thankifi.Common.Filters.Abstractions/IFilterService.cs
+++ src/Thankifi.Common.Filters.Abstractions/IFilterService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
@@ -29,5 +31,24 @@ namespace Thankifi.Common.Filters.Abstractions
         /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns>The transformed string or default if something went wrong..</returns>
         Task<string?> ApplyOrDefault(string filter, string str, CancellationToken cancellationToken = default);
+        
+        /// <summary>
+        /// Applies one or more filters to a string given a filter identifier and an input string.
+        /// </summary>
+        /// <param name="filters">The list with the identifiers of the <see cref="IFilter"/>s to apply.</param>
+        /// <param name="str">The incoming string.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <exception cref="InvalidFilterException">When the requested filter does not exists.</exception>
+        /// <returns>The transformed string.</returns>
+        Task<string> Apply(IEnumerable<string> filters, string str, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Applies one or more filter to a string given a filter identifier and an input string.
+        /// </summary>
+        /// <param name="filters">The list with the identifiers of the <see cref="IFilter"/>s to apply.</param>
+        /// <param name="str">The incoming string.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>The transformed string or default if something went wrong..</returns>
+        Task<string?> ApplyOrDefault(IEnumerable<string> filters, string str, CancellationToken cancellationToken = default);
     }
 }
